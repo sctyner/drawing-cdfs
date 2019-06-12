0856057424ab2d9a450ce5a5044fe07dfe4ec8b2
Sam Tyner <sctyner@iastate.edu>
2019-06-12 17:05:58 -0400

    second

--- app.R
+++ app.R
@@ -5,7 +5,8 @@
 # Find out more about building applications with Shiny here:
 #
 #    http://shiny.rstudio.com/
-#
+# https://shiny.rstudio.com/tutorial/
+# ended at about 22 minutes
 
 library(shiny)
 
